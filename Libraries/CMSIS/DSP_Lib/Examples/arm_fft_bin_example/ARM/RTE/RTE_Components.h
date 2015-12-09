3c7221724fdb85fa3de772ff305e6f1c2cbb4a3a
Roemerwall <hendrik.schmuelling@web.de>
2015-12-09 18:40:39 +0100

    First Commit

---
+++ Libraries/CMSIS/DSP_Lib/Examples/arm_fft_bin_example/ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'arm_cortexM4lf_fft_bin_example' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
